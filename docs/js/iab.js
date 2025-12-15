6a7556bf3c7051db53f52098400ba7c64b4e54ce
TamerMansour-AI <teamo.palestine@gmail.com>
2025-12-15 23:20:31 +0200

    Add SVG OG image and update SEO metadata

--- docs/js/iab.js
+++ docs/js/iab.js
@@ -1,5 +1,7 @@
 (function () {
-  const ua = navigator.userAgent || "";
-  const isIAB = /FBAN|FBAV|FB_IAB|Instagram/i.test(ua);
-  if (isIAB) document.documentElement.classList.add("iab-fb");
+  try {
+    const ua = navigator.userAgent || "";
+    const isIAB = /FBAN|FBAV|FB_IAB|Instagram/i.test(ua);
+    if (isIAB) document.documentElement.classList.add("iab-fb");
+  } catch (_) {}
 })();
